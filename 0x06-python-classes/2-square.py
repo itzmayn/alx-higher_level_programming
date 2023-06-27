330e716a0b1a92d2f3730495b5cfbae83dd1632c
Mayn <123576452+itzmayn@users.noreply.github.com>
2023-06-27 19:36:15 +0000

    m

--- 0x06-python-classes/2-square.py
+++ 0x06-python-classes/2-square.py
@@ -1,21 +1,14 @@
 #!/usr/bin/python3
+"""Square generation module for Python project 0x06
+"""
+
 
 class Square:
-    """A class representing a square"""
-    
+    """class defined for square generation
+    """
     def __init__(self, size=0):
-        """
-        Initializes the square with a given size.
-
-        Args:
-        size (int): The size of the square.
-
-        Returns:
-        None
-        """
-
         if type(size) is not int:
-            raise TypeError("size must be an integer")
-        elif size < 0:
-            raise ValueError("size must be >= 0")
+            raise TypeError('size must be an integer')
+        if size < 0:
+            raise ValueError('size must be >= 0')
         self.__size = size
