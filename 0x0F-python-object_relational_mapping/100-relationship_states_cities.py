f233b67fa05f9a3e5926455d6e2f161f915d6161
Mayn <123576452+itzmayn@users.noreply.github.com>
2023-09-17 21:23:16 +0000

    m

--- 0x0F-python-object_relational_mapping/100-relationship_states_cities.py
+++ 0x0F-python-object_relational_mapping/100-relationship_states_cities.py
@@ -1,22 +1,31 @@
 #!/usr/bin/python3
-# Creates the State “California” with the City “San Francisco”
-# from the database hbtn_0e_100_usa.
-# Usage: ./100-relationship_states_cities.py <mysql username> /
-#                                            <mysql password> /
-#                                            <database name>
+"""
+Script that creates the State “California” with the City “San Francisco” in the database hbtn_0e_100_usa
+"""
+
 import sys
 from sqlalchemy import create_engine
 from sqlalchemy.orm import sessionmaker
-from relationship_state import State
-from relationship_city import Base, City
+from relationship_state import State, Base
+from relationship_city import City
 
-if __name__ == "__main__":
-    engine = create_engine("mysql+mysqldb://{}:{}@localhost/{}"
-                           .format(sys.argv[1], sys.argv[2], sys.argv[3]),
-                           pool_pre_ping=True)
+if __name__ == '__main__':
+    if len(sys.argv) != 4:
+        print("Usage: ./100-relationship_states_cities.py <mysql username> <mysql password> <database name>")
+        sys.exit(1)
+
+    username, password, database_name = sys.argv[1:4]
+
+    # Create the database connection
+    engine = create_engine(f'mysql+mysqldb://{username}:{password}@localhost:3306/{database_name}', pool_pre_ping=True)
     Base.metadata.create_all(engine)
-    Session = sessionmaker(bind=engine)
-    session = Session()
 
-    session.add(City(name="San Francisco", state=State(name="California")))
-    session.commit()
+    # Create a session
+    Session = sessionmaker(bind=engine)
+    with Session() as session:
+        # Create the State "California" and the City "San Francisco"
+        california = State(name="California")
+        san_francisco = City(name="San Francisco", state=california)
+        session.add(california)
+        session.add(san_francisco)
+        session.commit()
