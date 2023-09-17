a587ba16287f87483cf621e1411e21156fe5309e
Mayn <123576452+itzmayn@users.noreply.github.com>
2023-09-17 21:18:11 +0000

    m

--- 0x0F-python-object_relational_mapping/10-model_state_my_get.py
+++ 0x0F-python-object_relational_mapping/10-model_state_my_get.py
@@ -14,30 +14,28 @@ from sqlalchemy import create_engine
 
 def print_state_id_by_name(username, password, database_name, state_name):
     try:
+        # Create the database connection
         engine = create_engine(f'mysql+mysqldb://{username}:{password}@localhost:3306/{database_name}', pool_pre_ping=True)
         Session = sessionmaker(bind=engine)
         Base.metadata.create_all(engine)
 
         # Create a session
-        session = Session()
+        with Session() as session:
+            # Extract the state by name
+            state = session.query(State).filter(State.name == state_name).one_or_none()
 
-        # Extract the state by name
-        state = session.query(State).filter(State.name == state_name).one_or_none()
-
-        # Print state ID
-        if state is None:
-            print("Not found")
-        else:
-            print(state.id)
+            # Print state ID
+            if state is None:
+                print("State not found")
+            else:
+                print("State ID:", state.id)
     except Exception as e:
         print("An error occurred:", str(e))
-    finally:
-        session.close()
 
 if __name__ == '__main__':
     if len(sys.argv) != 5:
         print("Usage: ./script.py <mysql username> <mysql password> <database name> <state name>")
         sys.exit(1)
 
-    username, password, database_name, state_name = sys.argv[1], sys.argv[2], sys.argv[3], sys.argv[4]
+    username, password, database_name, state_name = sys.argv[1:5]
     print_state_id_by_name(username, password, database_name, state_name)
--- 0x0F-python-object_relational_mapping/11-model_state_insert.py
+++ 0x0F-python-object_relational_mapping/11-model_state_insert.py
@@ -1,34 +1,38 @@
 #!/usr/bin/python3
 """
-    A script that adds the State object 'Louisiana' to hbtn_0e_6_usa
-    Username, password, dbname will be passed as arguments to the script.
+A script that adds the State object 'Louisiana' to hbtn_0e_6_usa
+Username, password, dbname will be passed as arguments to the script.
 """
-
 
 import sys
 from model_state import Base, State
 from sqlalchemy.orm import sessionmaker
 from sqlalchemy import create_engine
 
+def add_louisiana_state(username, password, database_name):
+    try:
+        # Create the database connection
+        engine = create_engine(f'mysql+mysqldb://{username}:{password}@localhost:3306/{database_name}', pool_pre_ping=True)
+        Session = sessionmaker(bind=engine)
+        Base.metadata.create_all(engine)
+
+        # Create a session
+        with Session() as session:
+            # Create the State object and add it to the database
+            new_state = State(name='Louisiana')
+            session.add(new_state)
+            session.commit()
+
+            # Retrieve and print the state ID
+            state_add = session.query(State).filter(State.name == 'Louisiana').one()
+            print("State 'Louisiana' added with ID:", state_add.id)
+    except Exception as e:
+        print("An error occurred:", str(e))
+
 if __name__ == '__main__':
-    engine = create_engine('mysql+mysqldb://{}:{}@localhost:3306/{}'.format(
-                           sys.argv[1], sys.argv[2], sys.argv[3]),
-                           pool_pre_ping=True)
+    if len(sys.argv) != 4:
+        print("Usage: ./script.py <mysql username> <mysql password> <database name>")
+        sys.exit(1)
 
-    Session = sessionmaker(bind=engine)
-
-    Base.metadata.create_all(engine)
-
-    # create a session
-    session = Session()
-
-    # create the object and add it
-    new_state = State(name='Louisiana')
-    session.add(new_state)
-    session.commit()
-
-    # print state.id
-    state_add = session.query(State).filter(State.name == 'Louisiana').one()
-    print(state_add.id)
-
-    session.close()
+    username, password, database_name = sys.argv[1:4]
+    add_louisiana_state(username, password, database_name)
