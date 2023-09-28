e7ac676852a5fe1f5f800aa514661282808bddd3
Mayn <123576452+itzmayn@users.noreply.github.com>
2023-09-28 21:25:42 +0000

    m

--- 0x10-python-network_0/1-body.sh
+++ 0x10-python-network_0/1-body.sh
@@ -1,18 +1,3 @@
 #!/bin/bash
-
-if [ $# -eq 0 ]; then
-  echo "Usage: $0 <URL>"
-  exit 1
-fi
-
-url=$1
-
-# Send a GET request to the URL and filter the body if the status code is 200
-curl -s -o /dev/null -w "%{http_code}" "$url" | {
-    read -r status_code
-    if [ "$status_code" -eq 200 ]; then
-        curl -s "$url"
-    else
-        echo "HTTP status code: $status_code"
-    fi
-}
+# sends GET request to the URL and displays the body of the response
+curl -sfL "$1" -X GET
