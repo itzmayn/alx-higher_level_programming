145b07cd76fc170e9ad81ef55d35267db7c27c2a
Mayn <123576452+itzmayn@users.noreply.github.com>
2023-09-28 21:12:03 +0000

    m

--- 0x10-python-network_0/101-post_json.sh
+++ 0x10-python-network_0/101-post_json.sh
@@ -1,8 +1,3 @@
 #!/bin/bash
-
-if [ $# -lt 2 ]; then
-  echo "Usage: $0 <URL> <JSON_FILENAME>"
-  exit 1
-fi
-
-curl -sX POST "$1" -H "Content-Type: application/json" -d @"$2" -L
+# sends a JSON POST request to a URL passed as the first argument, and displays the body of the response
+curl -sX POST $1 -H "Content-Type: application/json" -d @$2 -L
