112f5736de111e30d6958897ce793f10af1e53f4
Mayn <123576452+itzmayn@users.noreply.github.com>
2023-09-28 21:39:19 +0000

    m

--- 0x10-python-network_0/1-body.sh
+++ 0x10-python-network_0/1-body.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
-# sends GET request to the URL and displays the body of the response
-curl -sfL "$1" -X GET
+# takes in a URL, sends a request to that URL, and displays the size of the body of the response
+curl -sX GET $1 -L
