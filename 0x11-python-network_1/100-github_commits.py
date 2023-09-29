d3c5dbfd0c68134b3e18fc08ee7c8a23fdabce9f
Mayn <123576452+itzmayn@users.noreply.github.com>
2023-09-29 16:43:51 +0000

    m

--- 0x11-python-network_1/100-github_commits.py
+++ 0x11-python-network_1/100-github_commits.py
@@ -1,45 +1,22 @@
 #!/usr/bin/python3
 """
-Python script that fetches and displays information about the latest 10 commits
-from a GitHub repository.
+Python script that takes 2 arguments in order to solve this challenge.
 
-Usage: ./100-github_commits.py <repository_name> <repository_owner>
+Usage: ./100-github_commits.py <repository name> <repository owner>
 """
-
-import sys
+from sys import argv
 import requests
 
-def fetch_github_commits(repo_name, repo_owner):
-    # Construct the GitHub API URL for the commits of the specified repository
-    url = f"https://api.github.com/repos/{repo_owner}/{repo_name}/commits"
-
-    try:
-        # Send a GET request to the GitHub API
-        response = requests.get(url)
-
-        # Check if the request was successful (status code 200)
-        if response.status_code == 200:
-            commits = response.json()
-            
-            # Display information for the latest 10 commits
-            for commit in commits[:10]:
-                sha = commit.get("sha")
-                author_name = commit.get("commit").get("author").get("name")
-                print(f"{sha}: {author_name}")
-        else:
-            print(f"Error: Unable to fetch commits. Status code: {response.status_code}")
-    
-    except requests.exceptions.RequestException as e:
-        print(f"Error: {e}")
 
 if __name__ == "__main__":
-    # Check if the correct number of command-line arguments is provided
-    if len(sys.argv) != 3:
-        print("Usage: ./100-github_commits.py <repository_name> <repository_owner>")
-        sys.exit(1)
+    url = "https://api.github.com/repos/{}/{}/commits".format(argv[2], argv[1])
+    req = requests.get(url)
+    commits = req.json()
 
-    repo_name = sys.argv[1]
-    repo_owner = sys.argv[2]
-
-    fetch_github_commits(repo_name, repo_owner)
-
+    try:
+        for i in range(10):
+            print("{}: {}".format(
+                commits[i].get("sha"),
+                commits[i].get("commit").get("author").get("name")))
+    except IndexError:
+        pass
